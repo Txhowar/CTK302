57e1c6a7dfa87bf2420304caf0268676f6c5e263
Txhowar <txhowar@ilstu.edu>
2022-01-25 11:20:50 -0600

    Upload image project

--- Unit1/hello/sketch.js
+++ Unit1/hello/sketch.js
@@ -4,7 +4,7 @@ let ult;
 
 function setup() {
   createCanvas(500, 500);
-  textAlign(CENTER) ;
+
 
   azu = loadImage("assets/azumarill.jpg");
   tac = loadImage("assets/tacos.jpg");
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-  image(azu, width/2, 100);
-  image(tac, width/2, 200);
-  image(ult, width/2, 300);
+  image(azu, width/2, 100, 100, 100);
+  image(tac, width/2, 200, 100, 100);
+  image(ult, width/2, 300, 100, 100);
 }
--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -1,7 +1,18 @@
+let azu;
+let tac;
+let ult;
+
 function setup() {
   createCanvas(500, 500);
+
+
+  azu = loadImage("assets/azumarill.jpg");
+  tac = loadImage("assets/tacos.jpg");
+  ult= loadImage("assets/ultimate.jpg");
 }
 
 function draw() {
-
+  image(azu, width/2, 100, 100, 100);
+  image(tac, width/2, 200, 100, 100);
+  image(ult, width/2, 300, 200, 100);
 }
