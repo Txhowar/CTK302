61e5d5b22c2289a6ce851b5a71607613d33c03d2
Txhowar <txhowar@ilstu.edu>
2022-01-25 11:05:02 -0600

    hERE

--- Unit1/hello/sketch.js
+++ Unit1/hello/sketch.js
@@ -1,11 +1,18 @@
+let azu;
+let tac;
+let ult;
+
 function setup() {
   createCanvas(500, 500);
   textAlign(CENTER) ;
+
+  azu = loadImage("assets/azumarill.jpg");
+  tac = loadImage("assets/tacos.jpg");
+  ult= loadImage("assets/ultimate.jpg");
 }
 
 function draw() {
-  background('red') ;
-  fill('white') ;
-  textSize(32) ;
-  text("hello world!", 250, 250) ;
+  image(azu, width/2, 100);
+  image(tac, width/2, 200);
+  image(ult, width/2, 300);
 }
