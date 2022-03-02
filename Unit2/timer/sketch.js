af98bfbc1a8e8f8ec3313ed3c279655f0a6cf4ad
Txhowar <txhowar@ilstu.edu>
2022-03-02 17:47:55 -0600

    Timer update

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -13,7 +13,7 @@ function draw() {
 
     case 0:
       background("grey");
-      text("CLICK TO START", 800, 400);
+      text("WAIT TO START", 800, 400);
       //timer code
       timer++;
       if (timer > 3 * 60) {
@@ -24,8 +24,13 @@ function draw() {
 
     case 1:
       text("CASE 1", 100, 100);
+      fill('#8A39E1');
+    ellipse(660, 580, 150, 325);
+
+    fill('#B667F1');
+   ellipse(660, 580, 120, 295);
       timer++;
-      if (timer > 5 * 60) {
+      if (timer > 3 * 60) {
         timer = 0;
         state = 2;
       }
@@ -34,11 +39,74 @@ function draw() {
 
     case 2:
       text("CASE 2", 100, 100);
+      fill('#8A39E1');
+      quad(425, 580, 490, 500, 425, 420, 360, 500);
+
+      fill('black');
+      quad(425, 550, 460, 500, 425, 450, 390, 500);
+
+      fill('#8A39E1');
+        quad(500, 740, 450, 550, 400, 550, 350, 740);
+
+        fill("black");
+        rect(425, 545, 1, 190);
+
+        fill('#8A39E1');
+      ellipse(660, 580, 150, 325);
+
+      fill('#B667F1');
+      ellipse(660, 580, 120, 295);
       timer++;
-      if (timer > 5 * 60) {
+      if (timer > 3 * 60) {
         timer = 0;
-        state = 0;
+        state = 3;
       }
       break;
+
+      case 3:
+        text("CASE 3", 100, 100);
+        quad(425, 580, 490, 500, 425, 420, 360, 500);
+
+        fill('black');
+        quad(425, 550, 460, 500, 425, 450, 390, 500);
+
+        fill('#8A39E1');
+          quad(500, 740, 450, 550, 400, 550, 350, 740);
+
+          fill("black");
+          rect(425, 545, 1, 190);
+
+        fill('#8A39E1');
+      ellipse(660, 580, 150, 325);
+
+      fill('#B667F1');
+     ellipse(660, 580, 120, 295);
+        timer++;
+        if (timer > 3 * 60) {
+          timer = 0;
+          state = 4;
+        }
+          break;
+
+        case 4:
+        fill('#8A39E1');
+          text("Teleportation Successful", 700, 100);
+          fill('#8A39E1');
+          quad(425, 580, 490, 500, 425, 420, 360, 500);
+
+          fill('black');
+          quad(425, 550, 460, 500, 425, 450, 390, 500);
+
+          fill('#8A39E1');
+            quad(500, 740, 450, 550, 400, 550, 350, 740);
+
+            fill("black");
+            rect(425, 545, 1, 190);
+          timer++;
+          if (timer > 3 * 60) {
+            timer = 0;
+            state = 5;
+          }
+          break;
   }
 }
